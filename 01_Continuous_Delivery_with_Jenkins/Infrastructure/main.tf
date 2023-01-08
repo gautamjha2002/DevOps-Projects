d4aa7c6fbe19c86b72e4f6e450fe5f80aa07d602
Gautam Jha <gautamjha3112002@gmail.com>
2023-01-08 14:41:50 +0530

    Added Dockerfile and compose

--- 01_Continuous_Delivery_with_Jenkins/Infrastructure/main.tf
+++ 01_Continuous_Delivery_with_Jenkins/Infrastructure/main.tf
@@ -1,3 +1,71 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_security_group" "Jenkins-SG" {
+  name = "Jenkins-sg"
+  description = "This is a security group for Jenkins"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "jenkins" {
+  ami = "ami-06878d265978313ca"
+  instance_type = "t2.medium"
+  key_name = "Jenkins"
+  security_groups = ["Jenkins-sg"]
+
+  tags = {
+    Name = "Jenkins-Node"
+  }
+  user_data = <<EOF
+#!/bin/bash
+sudo apt-get update -y
+sudo apt install openjdk-11-jre -y
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
+EOF
+}
